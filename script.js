c75b08192e4b6507dd6468c01398a18edcc49006
vlad-nechiporchuk <nechyporchuk@ukr.net>
2019-02-22 23:22:54 +0200

    Add some logic

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
+let pizzaPrice = 0;
+let additionalPrice = 0;
+console.log(additionPrice)
+
 $('.objectsize').on('change', (()=>{
     $(event.target.nextElementSibling).text(event.target.value + " грн")
     const obj = {
-        pidor: event.target.name,
-        sam_pidor: event.target.value + 'grn',
+        Имя: event.target.name,
+        Цена: event.target.value + ' грн',
     }
     console.log(obj)
 }))
 
+$('.zakaz-test').click(function(){
+        const label = $(event.target);
+        const chosenPizzaPrice = +label.siblings('.inline').find('.price')[0].innerHTML.substring(0,3)
+        pizzaPrice = chosenPizzaPrice
+        $('#totalPrice').text(chosenPizzaPrice + ' грн')
+});
+
+$('.addition-price-input').on('change', (()=>{
+    const fixPrice = +event.target.name
+    let additionalItemPrice = fixPrice * event.target.value
+    additionalPrice += fixPrice
+    const a = $(event.target).parent('td').siblings('td')[2]
+    $(a).text(+additionalItemPrice + 'грн')
+    $('#additionPrice').text(additionalPrice + 'грн')
+    $('#totalPrice').text(+pizzaPrice + +additionalPrice + ' грн')
+
+}))
+
 $(document).ready(function () {
     $("#zakaz").click(function(){
+        $('.addition-price-input').each((index,item)=>{
+            console.log($(item))
+            $(item).val(0)
+        })
         $(".oformzakaz").fadeIn(500, function(){
             $(this).css("display", "block");
         });
+
         $(".form").fadeIn(500, function(){
             $(this).css("display", "flex");
         });
@@ -32,4 +59,19 @@ $(document).ready(function () {
             $(this).css("display", "flex");
         });
     });
+
+
+    $(".input_value").click(function (){
+        var price_id = $(this).parent().prev().children().attr("id");
+        var class_name = $(".price-pizza-" + price_id);
+        var input_value = $(this).val() * $(class_name).text();
+        $(".total_price").text(input_value + " грн");
+    });
+    $(".input_value2").click(function (){
+        var price_id = $(this).parent().prev().children().attr("id");
+        var class_name = $(".price-pizza-" + price_id);
+        var input_value = $(this).val() * $(class_name).text();
+        $(".total_price2").text(input_value + " грн");
+    });
 });
+
